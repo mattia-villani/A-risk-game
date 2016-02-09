b70253beb35466b30e3c8c84fa89cbd5dc492d0b
mattia.villani <mattiavillani94@gmail.com>
2016-02-09 17:01:36 +0100

    added mapshower test and maprenderer

---
+++ src/gui/map/MapRenderer.java
@@ -0,0 +1,21 @@
+package gui.map;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JComponent;
+
+public class MapRenderer extends JComponent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void paintComponent(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
+	 	 // drawing instructions
+	}
+	
+}
---
+++ src/tests/MapShower.java
@@ -0,0 +1,18 @@
+package tests;
+
+import javax.swing.*;
+
+import gui.map.MapRenderer;
+
+public class MapShower {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setSize(300,400);
+		frame.setTitle("Just visualize the map");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.add(new MapRenderer());
+	}
+
+}
