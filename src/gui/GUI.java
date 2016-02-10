28e526a08b019324e4ed35df1ec5822c8296cb1e
Andrew Kilbride <andykill@gmail.com>
2016-02-10 12:54:49 +0000

    overlayed world rendered over map image, unaligned atm

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,9 +1,9 @@
 package gui;
-
-import java.awt.BorderLayout;
+import java.awt.*;
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
+
 
 import core.WorldBuilder;
 import gui.map.MapRenderer;
@@ -23,29 +23,37 @@ public class GUI implements ActionListener {
 		uiFrame.getContentPane().setLayout(null);
 		uiFrame.setResizable(false);
 		
-		//
-		// Placeholder for the map 
-		/*
+		// Background Map
 		JLabel Map = new JLabel();
 		Map.setBounds(0, 0, 1000, 600);
 		Map.setIcon(new ImageIcon("images/PlaceholderMap.jpg"));
-		uiFrame.getContentPane().add(Map);*/
-		
+	    
+		// Node Map
 		JComponent worldMap = new MapRenderer( WorldBuilder.Build() );
 		worldMap.setBounds(0, 0, 1000, 600);
-		uiFrame.getContentPane().add(worldMap);
+		worldMap.setOpaque(false);
 		
+		// Combination of Background and Node maps, alignments still off
+		JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0,0, 1000, 600);
+        layeredPane.add(worldMap, 0);
+        layeredPane.add(Map, 1);
+        uiFrame.getContentPane().add(layeredPane);
+
+		// Text log area
 		textLog = new JTextArea(50, 100);
 		textLog.setBounds(0, 600, 1000, 65);
 		textLog.setEditable(false);
 		JScrollPane scrollPane = new JScrollPane(textLog);	
 		uiFrame.getContentPane().add(textLog);
 		
+		// Input area
 		textInput = new JTextField();
 		textInput.setBounds(0, 665, 800, 35);
 		uiFrame.getContentPane().add(textInput);
 		textInput.setColumns(10);
 		
+		// Submit button, non focusable, default action for enter key
 		submitButton = new JButton("Submit");
 		submitButton.addActionListener(this); 
 		submitButton.setBounds(798, 665, 200, 35);
@@ -58,6 +66,5 @@ public class GUI implements ActionListener {
 	
 	public void actionPerformed(ActionEvent e) {
         // Button actions
-    };
-	
+    };	
 }
