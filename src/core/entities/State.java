42626406fcc936826db7e2826daf0ed09075ad88
mattia.villani <mattiavillani94@gmail.com>
2016-02-10 01:12:15 +0100

    fancy drawing of state and archs

--- src/core/entities/State.java
+++ src/core/entities/State.java
@@ -1,5 +1,9 @@
 package core.entities;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import core.Constants;
 
 abstract public class State {
@@ -8,6 +12,7 @@ abstract public class State {
 	protected String name;
 	protected int x,y;
 	protected int[] adjacent;
+	protected List<State> smaller_adjacent;
 	protected Continent continent;
 	
 	public State (int i){ init(i); }
@@ -15,4 +20,14 @@ abstract public class State {
 
 	public int getX(){ return x; }
 	public int getY(){ return y; }
+	public Continent getContinet(){ return continent; }
+	public List<State> getSmallerAdjacent(World world){
+		if ( smaller_adjacent != null ) return smaller_adjacent;
+		List<State> l = new ArrayList<>();
+		for (int i=0;i<adjacent.length;i++)
+			if ( adjacent[i] < index ) 
+				l.add(world.getState( adjacent[i] ));
+		smaller_adjacent = Collections.unmodifiableList(l);
+		return smaller_adjacent;
+	}
 }
--- src/gui/map/GraphDrawer.java
+++ src/gui/map/GraphDrawer.java
@@ -1,7 +1,10 @@
 package gui.map;
 
 import java.awt.Color;
+import java.awt.GradientPaint;
 import java.awt.Graphics2D;
+import java.awt.LinearGradientPaint;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
 import core.entities.Continent;
@@ -9,22 +12,55 @@ import core.entities.State;
 import core.entities.World;
 
 public class GraphDrawer {
-	static final int circle_ray = 4;
+	static final float margin = 0.8f;
+	static final int circle_ray = 10;
+	static final int thickness = 8;
 	static BufferedImage graph;
 	
+	static public void drawStateValues(Graphics2D g, State s){
+		
+	}
+	
 	static public void drawGraph( Graphics2D comp_g, World world, int width, int height ){
-		if ( graph == null || width>graph.getWidth() || height>graph.getHeight() ){
+		if ( graph == null || width>graph.getWidth() || height>graph.getHeight() || width<graph.getWidth()*margin || height<graph.getHeight()*margin ){
 			graph = new BufferedImage ( width, height, BufferedImage.TYPE_INT_ARGB );
 			Graphics2D g = (Graphics2D) graph.getGraphics();
+			g.scale( ((double)width)/(double)World.VIRTUAL_WIDTH, ((double)height)/(double)World.VIRTUAL_HEIGHT);
 			
-			g.setColor( Color.RED );
-			for (Continent c:world.getContinents())
-				for (State s:c){
+			for (Continent cont:world.getContinents()){
+				for (State s:cont){
+					g.setColor( cont.getColor() );
+					for (State adj:s.getSmallerAdjacent(world)){
+						int a=s.getX(), b=s.getY();
+						int c=adj.getX(), d=adj.getY();
+						if ( s.getContinet() != adj.getContinet() ){
+							if ( Math.abs(a-c) > world.VIRTUAL_WIDTH/2 ){
+								// WORNING!!! this fix works only with the traditional map
+								int sign = (int)Math.signum(a-c);
+								int A = a-sign*world.VIRTUAL_WIDTH;
+								int C = c+sign*world.VIRTUAL_WIDTH;
+								int fixed_a = A<0? 0: (A>world.VIRTUAL_WIDTH?world.VIRTUAL_WIDTH:A);
+								int fixed_c = C<0? 0: (C>world.VIRTUAL_WIDTH?world.VIRTUAL_WIDTH:C);
+								g.setPaint(new GradientPaint(a,b, s.getContinet().getColor(), fixed_c, d, adj.getContinet().getColor()));
+								g.drawLine( a, b, C, d );																	
+								g.setPaint(new GradientPaint(fixed_a,b, s.getContinet().getColor(), c, d, adj.getContinet().getColor()));
+								g.drawLine( A, b, c, d );																	
+							}else{
+								g.setPaint(new GradientPaint(a,b, s.getContinet().getColor(), c, d, adj.getContinet().getColor()));
+								g.drawLine( a, b, c, d );
+							}
+							g.setColor( cont.getColor() );							
+						}else
+							g.drawLine( a, b, c, d );
+					}
 					g.fillOval(s.getX()-circle_ray, s.getY()-circle_ray, 2*circle_ray, 2*circle_ray);
+					drawStateValues(g,s);
 				}
+			}
 			
 			g.dispose();
 		}
+		
 		if ( comp_g != null )
 			comp_g.drawImage(graph, 0, 0, World.VIRTUAL_WIDTH, World.VIRTUAL_HEIGHT, null );
 	}
