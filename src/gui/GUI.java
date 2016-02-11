4adc540bbc1c33b3401ea92507d75fa1f0038f37
carneyjimmy <carneyjimmy@wustl.edu>
2016-02-11 12:33:05 +0000

    get names

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -12,33 +12,33 @@ public class GUI implements ActionListener {
 	private JFrame uiFrame;
 	public static JTextArea textLog;
 	public static JTextField textInput;
-	private JButton submitButton;
-	
+	public static JButton submitButton;
 
-		public void createGUI() {	
+
+	public void createGUI() {	
 		uiFrame=new JFrame();
 		uiFrame.setBounds(0, 0, 1000,728);
 		uiFrame.setTitle("Risk: The Game of Software Engineering");
 		uiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		uiFrame.getContentPane().setLayout(null);
 		uiFrame.setResizable(false);
-		
+
 		// Background Map
 		JLabel Map = new JLabel();
 		Map.setBounds(0, 0, 1000, 600);
 		Map.setIcon(new ImageIcon("images/PlaceholderMap.jpg"));
-	    
+
 		// Node Map
 		JComponent worldMap = new MapRenderer( WorldBuilder.Build() );
 		worldMap.setBounds(0, 0, 1000, 600);
 		worldMap.setOpaque(false);
-		
+
 		// Combination of Background and Node maps, alignments still off
 		JLayeredPane layeredPane = new JLayeredPane();
-        layeredPane.setBounds(0,0, 1000, 600);
-        layeredPane.add(worldMap, 0);
-        layeredPane.add(Map, 1);
-        uiFrame.getContentPane().add(layeredPane);
+		layeredPane.setBounds(0,0, 1000, 600);
+		layeredPane.add(worldMap, 0);
+		layeredPane.add(Map, 1);
+		uiFrame.getContentPane().add(layeredPane);
 
 		// Text log area
 		textLog = new JTextArea(50, 100);
@@ -46,13 +46,13 @@ public class GUI implements ActionListener {
 		textLog.setEditable(false);
 		JScrollPane scrollPane = new JScrollPane(textLog);	
 		uiFrame.getContentPane().add(textLog);
-		
+
 		// Input area
 		textInput = new JTextField();
 		textInput.setBounds(0, 665, 800, 35);
 		uiFrame.getContentPane().add(textInput);
 		textInput.setColumns(10);
-		
+
 		// Submit button, non focusable, default action for enter key
 		submitButton = new JButton("Submit");
 		submitButton.addActionListener(this); 
@@ -60,11 +60,12 @@ public class GUI implements ActionListener {
 		submitButton.setFocusable(false);
 		uiFrame.getContentPane().add(submitButton);
 		uiFrame.getRootPane().setDefaultButton(submitButton);
-		
+
 		uiFrame.setVisible(true);
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
-        // Button actions
-    };	
+		// Button actions
+		System.out.println("hr");
+	};	
 }
--- src/main.java
+++ src/main.java
@@ -1,8 +1,40 @@
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import core.entities.Player;
+import gui.GUI;
 
 public class main {
-	/*hi*/
+	public static String player1Name;
+	public static String player2Name;
+	public static Player player1;
+	public static Player player2;
+	public static boolean didPress = false;
+	
 	public static void main(String[] args) {
 		System.out.println("Hello Wold!");
+		GUI window=new GUI();
+		window.createGUI(); //create frame
+		
+		
+	
+		while(!didPress){
+			gui.GUI.textLog.setText("Welcome! What is player 1's name?");
+		}
+		didPress = false;
+		player1Name = gui.GUI.textInput.getText();
+		gui.GUI.textInput.setText("");
+		while(!didPress){
+			gui.GUI.textLog.setText(player1Name + " will be blue. What is player two's name?");
+		}
+		String player2Name = gui.GUI.textInput.getText();
+		gui.GUI.textLog.setText(player2Name + " will be red");	
+
+			
+		
+	
+		
+		
 	}
 }
