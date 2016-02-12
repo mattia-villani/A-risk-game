8672aa2563bba762fb29ebaed2eeae1780f3718d
carneyjimmy <carneyjimmy@wustl.edu>
2016-02-12 12:44:19 +0000

    modularized main method

--- src/core/main.java
+++ src/core/main.java
@@ -11,96 +11,109 @@ import core.entities.World;
 import gui.GUI;
 
 public class main {
-  public static String player1Name;
-  public static String player2Name;
-  public static Player player1;
-  public static Player player2;
-  public static Player neut1;
-  public static Player neut2;
-  public static Player neut3;
-  public static Player neut4;
-  public static ArrayList <Player> players;
-  
-  public static boolean didPress = false;
-  public static World world;
-  
-  /*
-   * main method, set up board and then create frame
-   */
+	private static String player1Name;
+	private static String player2Name;
+	private static Player player1;
+	private static Player player2;
+	private static Player neut1;
+	private static Player neut2;
+	private static Player neut3;
+	private static Player neut4;
+	private static ArrayList <Player> players;
 
-  public static void main(String[] args) throws IOException {
-    
-    GUI window=new GUI();
-    //window.createGUI(); //create frame
-    
-    while(!didPress){
-      window.setLog("Welcome! What is player 1's name?");
-    }
-    didPress = false;
-    player1Name = window.getInput();
-    window.resetInput();
-    while(!didPress){
-      window.setLog(player1Name + " will be blue. What is player two's name?");
-    }
-    didPress = false;
-    String player2Name = window.getInput();
-    window.setLog(player2Name + " will be red. Lets begin!"); 
-    window.resetInput();
-    
-    // create players
-    player1 = new Player(player1Name, Color.blue);
-    players.add(player1);
-    player2 = new Player(player2Name, Color.red);
-    players.add(player2);
-    neut1 = new Player("neutral 1", Color.BLACK);
-    players.add(neut1);
-    neut2 = new Player("neutral 2", Color.green);
-    players.add(neut2);
-    neut3 = new Player("neutral 3", Color.darkGray);
-    players.add(neut3);
-    neut4 = new Player("neutral 4", Color.ORANGE);
-    players.add(neut4);
-    
-    world = WorldBuilder.Build();
-    createGame();
-    
-   
-    
-    world.reset_invalidate();
-    
-    
-    
-  }
-  
-  public static void createGame(){
-    int statesOwned = 0;
-    for (int i = 0; i < world.getStates().size(); i++){
-      //give each player 9, and each neutral 6
-      if(i < statesOwned + Constants.INIT_COUNTRIES_PLAYER){
-        statesOwned += Constants.INIT_COUNTRIES_PLAYER;
-        world.getState(i).setOwner(player1);        
-      }
-      else if(i < statesOwned + Constants.INIT_COUNTRIES_PLAYER){
-        statesOwned += Constants.INIT_COUNTRIES_PLAYER;
-        world.getState(i).setOwner(player2);        
-      }
-      else if(i < statesOwned + Constants.INIT_COUNTRIES_NEUTRAL){
-        statesOwned += Constants.INIT_COUNTRIES_NEUTRAL;
-        world.getState(i).setOwner(neut1);        
-      }
-      else if(i < statesOwned + Constants.INIT_COUNTRIES_NEUTRAL){
-        statesOwned += Constants.INIT_COUNTRIES_NEUTRAL;
-        world.getState(i).setOwner(neut2);        
-      }
-      else if(i < statesOwned + Constants.INIT_COUNTRIES_NEUTRAL){
-        statesOwned += Constants.INIT_COUNTRIES_NEUTRAL;
-        world.getState(i).setOwner(neut3);        
-      }
-      else if(i < statesOwned + Constants.INIT_COUNTRIES_NEUTRAL){
-        statesOwned += Constants.INIT_COUNTRIES_NEUTRAL;
-        world.getState(i).setOwner(neut4);        
-      }
-      world.getState(i).setArmy(1);
-    }
-  }
+	public static ArrayList<Player> getPlayers() {
+		return players;
+	}
+
+	public static void setPlayers(ArrayList<Player> players) {
+		main.players = players;
+	}
+
+	public static boolean didPress = false;
+	public static World world;
+
+	/*
+	 * main method, set up board and then create frame
+	 */
+
+	public static void main(String[] args) throws IOException {
+
+		GUI window=new GUI();
+		//window.createGUI(); //create frame
+
+		while(!didPress){
+			window.setLog("Welcome! What is player 1's name?");
+		}
+		didPress = false;
+		String player1Name = window.getInput();
+		window.resetInput();
+		while(!didPress){
+			window.setLog(player1Name + " will be blue. What is player two's name?");
+		}
+		didPress = false;
+		String player2Name = window.getInput();
+		window.setLog(player2Name + " will be red. Lets begin!"); 
+		window.resetInput();
+
+		// create players
+		createPlayers();
+
+		world = WorldBuilder.Build();
+
+		createGame();
+
+
+
+		world.reset_invalidate();
+
+
+
+	}
+	public static void createPlayers(){
+		player1 = new Player(player1Name, Color.blue);
+		players.add(player1);
+		player2 = new Player(player2Name, Color.red);
+		players.add(player2);
+		neut1 = new Player("neutral 1", Color.BLACK);
+		players.add(neut1);
+		neut2 = new Player("neutral 2", Color.green);
+		players.add(neut2);
+		neut3 = new Player("neutral 3", Color.darkGray);
+		players.add(neut3);
+		neut4 = new Player("neutral 4", Color.ORANGE);
+		players.add(neut4);
+	}
+	public static void createGame(){
+
+
+		int statesOwned = 0;
+		for (int i = 0; i < world.getStates().size(); i++){
+			//give each player 9, and each neutral 6
+			if(i < statesOwned + Constants.INIT_COUNTRIES_PLAYER){
+				statesOwned += Constants.INIT_COUNTRIES_PLAYER;
+				world.getState(i).setOwner(player1);        
+			}
+			else if(i < statesOwned + Constants.INIT_COUNTRIES_PLAYER){
+				statesOwned += Constants.INIT_COUNTRIES_PLAYER;
+				world.getState(i).setOwner(player2);        
+			}
+			else if(i < statesOwned + Constants.INIT_COUNTRIES_NEUTRAL){
+				statesOwned += Constants.INIT_COUNTRIES_NEUTRAL;
+				world.getState(i).setOwner(neut1);        
+			}
+			else if(i < statesOwned + Constants.INIT_COUNTRIES_NEUTRAL){
+				statesOwned += Constants.INIT_COUNTRIES_NEUTRAL;
+				world.getState(i).setOwner(neut2);        
+			}
+			else if(i < statesOwned + Constants.INIT_COUNTRIES_NEUTRAL){
+				statesOwned += Constants.INIT_COUNTRIES_NEUTRAL;
+				world.getState(i).setOwner(neut3);        
+			}
+			else if(i < statesOwned + Constants.INIT_COUNTRIES_NEUTRAL){
+				statesOwned += Constants.INIT_COUNTRIES_NEUTRAL;
+				world.getState(i).setOwner(neut4);        
+			}
+			world.getState(i).setArmy(1);
+		}
+	}
 }
