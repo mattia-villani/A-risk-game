2374080d87d40bf3c4ea9962afe89c0793835034
Andrew Kilbride <andykill@gmail.com>
2016-02-22 13:23:51 +0000

    basic deck and cards

---
+++ src/core/entities/TerritoryCard.java
@@ -0,0 +1,13 @@
+package core.entities;
+
+public class TerritoryCard {
+	private int index;
+	
+	public TerritoryCard(int index){
+		this.index = index;	
+	}
+	
+	public int getIndex(){
+		return index;
+	}
+}
---
+++ src/core/entities/TerritoryDeck.java
@@ -0,0 +1,43 @@
+package core.entities;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TerritoryDeck {
+	
+	private ArrayList<TerritoryCard> Deck;
+	private int top = 0;
+	
+	
+	public TerritoryDeck() {
+		Deck = new ArrayList<TerritoryCard>(42);
+		
+		for (int i=0; i<42; i++){
+			Deck.add(new TerritoryCard(i));
+		}
+		
+		Collections.shuffle(Deck);
+	}
+	
+	public ArrayList<TerritoryCard> getTerritoryDeck(){
+		return Deck;
+	}
+	
+	public int getTop(){
+		return top;
+	}
+	
+	public void shuffle(){
+		Collections.shuffle(Deck);
+	}
+
+	public void addTerritoryCard(int index){
+		Deck.add(new TerritoryCard (index));
+	}
+	
+	public TerritoryCard drawTerritoryCard(){
+		TerritoryCard topTerritoryCard = Deck.get(top);
+		top++;
+		return topTerritoryCard;	
+	}	
+}
