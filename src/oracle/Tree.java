7340000dd21f360747a30d2d74761bb2928202e8
mattia.villani <mattiavillani94@gmail.com>
2016-02-25 13:16:26 +0100

    oracle passing the tests

--- src/oracle/Tree.java
+++ src/oracle/Tree.java
@@ -1,29 +1,38 @@
 package oracle;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
-import javax.xml.stream.events.Characters;
 
 
 public class Tree {
+	static final private boolean verboseAdd = false;
+	static final private boolean verboseEvalue = false;
 	
-	int count = 0;
-	boolean finalNode = false;
-	Character nodeLabel ;
+	@SuppressWarnings("serial")
+	public class NotUniqueException extends RuntimeException{};
+	@SuppressWarnings("serial")
+	public class EmptyException extends RuntimeException{};
+	
+	public class EmptyTree extends Tree{
+		@Override
+		public String toString(){
+			return "EmpetyTree";
+		}
+		@Override
+		public String getUniquePath() throws NotUniqueException, EmptyException{
+			return "";
+		}
+	}
+	
 	private Map<Character, Tree> childs = new HashMap<>();
 	
-	public Tree ( char head, String tail ){
-		assert tail != null;
-		nodeLabel = head;
-		if ( ! tail.equals("") )
-			add(tail);
+	private Tree(){}
+	
+	public Tree ( String tail ){
+		assert tail != null && !tail.equals("") ;
+		add(tail);
 	}
 	
 	public Tree( Collection<String> input ){
@@ -36,56 +45,67 @@ public class Tree {
 		assert insertedAtLeastOne : "The collection was invalid or empty";
 	}
 	
-	public void setAsFinal(){
-		finalNode = true;
-	}
-	
 	public void add( String string ){
+		
+		if ( verboseAdd ) System.out.println("tree.add("+string+").pre "+this);
+
 		assert (string!=null && !string.equals("")) : "Why is string null or empty??";
 		char head = string.charAt(0);
 		String tail = string.substring(1);
 		Tree tree = null;
-		count ++;
-		if ( childs.containsKey(head) ) tree = childs.get(head);
-		else {
-			tree = new Tree(head, tail);
+		if ( childs.containsKey(head) ){ 
+			if ( ! tail.equals("") ) childs.get(head).add( tail );
+		} else {
+			tree = tail.equals("") ? null : new Tree(tail);
 			childs.put(head, tree);
 		}
-		if ( tail.equals("") ) tree.setAsFinal();
+		
+		if ( verboseAdd ) System.out.println("tree.add("+string+").post "+this);
 	}
 	
 	public Tree evalue( String string ){
+		if ( verboseEvalue ) System.out.println("tree.evalue("+string+").pre "+this);
+
 		assert string != null : "string should be not null";
-		assert !string.equals("") || finalNode : "string shouldn't be empty";
+
 		Tree result = null;
-		if ( string.equals("") && this.finalNode ) result = this;
+		
+		if ( string.equals("") ) result = this;
 		else{
 			char head = string.charAt(0);
 			String tail = string.substring(1);
-			if ( childs.containsKey(head) == false ) { /* not into */ }
+			if ( childs.containsKey(head) == false ) result = null;
 			else {
 				Tree tree = childs.get(head);
-				result = tree.evalue(tail);
+				result = (tree!=null) ? tree.evalue(tail) : new EmptyTree() ;
 			}
 		}
+
+		if ( verboseEvalue ) System.out.println("tree.evalue("+string+").post:result="+result+"; "+this);
 		return result;
 	}
-	public boolean isFinal(){
-		return finalNode;
-	}
-	public boolean isUniquePath(){
-		return count == 0;
-	}
-	public Character getChar(){
-		return nodeLabel;
-	}
 	
-	public String getUniquePath(){
-		if ( ! isUniquePath() ) throw new RuntimeException();
+	public String getUniquePath() throws NotUniqueException, EmptyException{
+		int childsNum = childs.size();
+		
+		if ( childsNum == 0 ) throw new EmptyException();
+		if ( childsNum != 1 ) throw new NotUniqueException();
+		
 		char key = childs.keySet().iterator().next();
 		Tree tree = childs.get(key);
-		if ( tree.isFinal() ) return tree.getChar().toString();
-		else return tree.getChar()+tree.getUniquePath();
+		
+		if ( tree == null ) return key+"";
+		else return key + tree.getUniquePath();
+	}
+	
+	
+	@Override 
+	public String toString(){
+		String result = "tree{ ";
+		for ( char key : childs.keySet() )
+			result += key+":"+childs.get(key)+" ";
+		result+="}";
+		return result;
 	}
 	
 }
--- src/tests/TestSudgestior.java
+++ src/tests/TestSudgestior.java
@@ -21,34 +21,46 @@ public class TestSudgestior {
 	public static void main(String[] args) {
 		
 		String[] dictionary = new String[]{
-			"abc", "abd", "abef", "abeg", "daf", "asd", "dsas"	
+			"abc", "abd", "hello world", "abef", "abeg", "daf", "asd", "dsas"	
 		};
 		
 		Set<String> words = new HashSet<>();
 		for ( String s : dictionary )
 			words.add(s);
 		
+		Tree root = new Tree( words );
+
+		System.out.println( "debug: "+root );
+		
 		Tree tree;
 		
-		Oracle oracle = new Oracle( new Tree( words ) );
+		Oracle oracle = new Oracle( root );
 				
 		try{
 			tree = oracle.evalue("abc");
-			if ( tree!=null && tree.isUniquePath() && "".equals(tree.getUniquePath()) ) yes(1);
+			if ( tree!=null && "".equals(tree.getUniquePath()) ) yes(1);
 			else no(1);
-		}catch(Exception e){ no(1); };
+		}catch(Exception e){ no(1); e.printStackTrace(); };
 
 		try{
 			tree = oracle.evalue("ab");
-			if ( tree!=null && !tree.isUniquePath() ) yes(2);
-			else no(2);
-		}catch(Exception e){ no(2); };
+			tree.getUniquePath();
+			no(2);
+		}catch(Tree.NotUniqueException e){ yes(2); }
+		catch(Exception e){ no(2); e.printStackTrace();};
 
 		try{
 			tree = oracle.evalue("errore");
-			if ( tree==null ) yes(3);
+			if ( tree == null ) yes(3);
 			else no(3);
-		}catch(Exception e){ no(3); };
+		}catch(Exception e){ no(3); e.printStackTrace();};
+
+		try{
+			tree = oracle.evalue("hello ");
+			String result = tree.getUniquePath();
+			if ( result.equals("world") ) yes(4);
+			else no(4);
+		}catch(Exception e){ no(4); e.printStackTrace();};
 
 
 	}
